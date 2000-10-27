66979d54c2123d43fae4907370afe78657cc94e4
Daniel Berger <djberg96@yahoo.com>
2000-10-27 13:43:10 +0000

    Import of DJBERG/Tk-JListbox-0.02 from CPAN.
    gitpan-cpan-distribution: Tk-JListbox
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DJBERG/Tk-JListbox-0.02.tar.gz
    gitpan-cpan-author:       DJBERG
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -8,6 +8,7 @@
 
 BEGIN { $| = 1; print "1..last_test_to_print\n"; }
 END {print "not ok 1\n" unless $loaded;}
+use Tk;
 use JListbox;
 $loaded = 1;
 print "ok 1\n";
@@ -18,17 +19,18 @@ print "ok 1\n";
 # (correspondingly "not ok 13") depending on the success of chunk 13
 # of the test code):
 
-# JListbox test program
-# JListbox test program
-use Tk;
-use JListbox;
+require Tk::DialogBox;
 
 $mw = MainWindow->new;
-$jlb = $mw->JListbox(-justify=>'right', -width=>75);
+
+$jlb = $mw->JListbox(-justify=>'center', -width=>75, -popupmenu=>1);
+
 $jlb->pack(-expand=>1, -fill=>'x');
 
-$jlb->insert('end',"Hello World");
-$jlb->insert('end',"Hello Once Again");
+$jlb->insert('end',"Hello World!");
+$jlb->insert('end',"How are you");
+$jlb->insert('end',"ABCDEFG");
+$jlb->insert('end',"12345");
 
 $cButton = $mw->Button(-text=>"Center Text", -command=>sub{
    $jlb->configure(-justify=>'center')}
@@ -45,4 +47,8 @@ $rButton->pack;
 $eButton->pack;
 
 MainLoop;
+
+
+
+
 
